7c31a5d049d4113679c48ae572adaafa21d7787f
mmoiseev <matt-hunter@ngs.ru>
2013-04-18 08:10:47 +0700

    Исправления по задаче 1

--- Task_1/src/Main.java
+++ Task_1/src/Main.java
@@ -1,17 +1,6 @@
 import java.util.Random;
 import java.util.Scanner;
 
-/**
- todo выкосить все комментарии, не относящиеся к делу
- todo мои замечания тут по мелочи, но они нужны
- todo как сделаете, поставлю галочку
-
- * Created with IntelliJ IDEA.
- * User: Matt
- * Date: 13.04.13
- * Time: 14:45
- * To change this template use File | Settings | File Templates.
- */
 public class Main {
     public static void main(String[] args) {
         Random random = new Random();
@@ -20,20 +9,16 @@ public class Main {
 
         System.out.println("Введите число от 0 до 10 " + " ответ " + numb);
 
-        for(int i = 0; i<5;){
+        for (int i = 0; i < 5; ) {
             int val;
-            try{
+            try {
                 val = scanner.nextInt();
-            } catch (Exception e){
+            } catch (Exception e) {
                 scanner.nextLine();
                 System.out.println("Введите число от 1 до 10!");
-                //e.printStackTrace(); todo и этот тоже выкосить.
-                // todo закомментированный код,
-                // todo который не исполняется вводит читающего в заблуждение, не
-                // todo понятно, зачем он нужен.
                 continue;
             }
-            if(val == numb){  // todo нужно отформатировать код (CTRL_ALT_L)
+            if (val == numb) {
                 System.out.println("Ура");
                 break;
             } else if (val > numb) {
@@ -41,10 +26,9 @@ public class Main {
             } else {
                 System.out.println("Загаданное число больше");
             }
-            System.out.println("Осталось попыток " + (5-++i));
-            // todo преинкримент это здорово, но мне кажется, что это затрудняет чтение
-            // todo чем проще программа, тем лучше. я бы вытащил наружу, но не настаиваю
-        }
+            System.out.println("Осталось попыток " + (4 - i));
+            i++;
+         }
 
         System.out.println("Правильный ответ " + numb);
 
